eeaf203702511e91ca3a289505bb9cb4877758a1
Steve <stephan_moser@gmx.de>
2021-08-02 02:37:10 +0200

    Expansion.TakistanPlus for Experimental

---
+++ Takistan/ExpansionCOM.TakistanPlus/core/BaseModuleInclude.c
@@ -0,0 +1,8 @@
+#include "$CurrentDir:missions\\ExpansionCOM.ChernarusPlus\\core\\ModuleManager.c"
+#include "$CurrentDir:missions\\ExpansionCOM.ChernarusPlus\\core\\StaticFunctions.c"
+
+static bool m_comGodMode = false;
+
+#include "$CurrentDir:missions\\ExpansionCOM.ChernarusPlus\\core\\WidgetStore.c"
+
+#include "$CurrentDir:missions\\ExpansionCOM.ChernarusPlus\\core\\CommunityOfflineClient.c"
